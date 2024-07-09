739b00b23d4bfad1a2fbb22a0ec6427d86c38ab0
Mei Huang <mei.h.hmi@gmail.com>
2024-07-09 13:18:54 +0800

    build: 新增redux

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "@/App";
 import "@/index.scss";
 import { RouterProvider } from "react-router-dom";
 import router from "@/router";
+import { Provider } from "react-redux";
+import store from "@/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router}>
-      <App />
-    </RouterProvider>
+    <Provider store={store}>
+      <RouterProvider router={router}>
+        <App />
+      </RouterProvider>
+    </Provider>
   </React.StrictMode>
 );
